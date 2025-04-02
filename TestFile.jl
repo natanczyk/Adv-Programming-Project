d7cdf6ca21a5ca8296274d15dce47140678feb55
RenskeAnna02 <renskeanna2@gmail.com>
2025-04-02 22:09:41 +0100

    tesfileeee

--- TestFile.jl
+++ TestFile.jl
@@ -6,10 +6,9 @@ reciprocal(x) =
         error(DivisionByZero()) :
         1/x
 
-
 handling(DivisionByZero => (c)->println("I saw it too")) do 
     handling(DivisionByZero => (c)->println("I saw a division by zero")) do
-        reciprocal(1)
+        reciprocal(0)
     end
 end
 
@@ -41,3 +40,33 @@ to_escape() do exit
         end
     end
 end
+
+to_escape() do exit
+    handling(DivisionByZero =>
+    (c)->println("I saw it too")) do
+        handling(DivisionByZero =>
+        (c)->(println("I saw a division by zero"); exit("Done"))) do
+            reciprocal(0)
+        end
+    end
+end
+
+mystery(n) =
+    1 +
+    to_escape() do outer
+        1 +
+        to_escape() do inner
+            1 +
+            if n == 0
+                inner(1)
+            elseif n == 1
+                outer(1)
+            else
+            1
+            end
+        end
+    end
+
+mystery(0)
+mystery(1)
+mystery(2)
