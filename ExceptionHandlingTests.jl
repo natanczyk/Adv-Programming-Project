2da3d75c3f396395c171e0b4e38210cc2affc9b1
Chiron <chiron.ricken@objectmind.de>
2025-04-04 20:11:05 +0100

    test modul läuft nicht!

---
+++ ExceptionHandlingTests.jl
@@ -0,0 +1,191 @@
+
+# Provides functions to test the exception handling
+module TestFunctionProvider
+
+    export  DivisionByZero, LineEndLimit, reciprocal_error, reciprocal_restart, reciprocal_signal, mystery, infinity, print_line
+
+    include("Exceptional.jl")
+    using .ExceptionHandling
+
+    # Define custom exception-types
+    struct DivisionByZero <: Exception end
+    struct LineEndLimit <: Exception end
+
+    reciprocal_error(x) =
+        x == 0 ?
+            ExceptionHandling.error(DivisionByZero()) :
+            1/x
+
+    reciprocal_signal(x) =
+        x == 0 ?
+            signal(DivisionByZero()) :
+            1/x
+
+    reciprocal_restart(value) =
+        with_restart(:return_zero => ()->0,
+                    :return_value => identity,
+                    :retry_using => reciprocal_restart) do
+            value == 0 ?
+                error(DivisionByZero()) :
+                1/value
+        end
+
+    infinity() =
+        with_restart(:just_do_it => ()->1/0) do
+            reciprocal_error(0)
+        end
+
+    print_line(str, line_end=10) =
+        let col = 0
+        for c in str
+            print(c)
+            col += 1
+                if col == line_end
+                    signal(LineEndLimit())
+                    col = 0
+                end
+            end
+        end
+
+    mystery(n) =
+        1 +
+        to_escape() do outer
+            1 +
+            to_escape() do inner
+                1 +
+                if n == 0
+                    inner(1)
+                elseif n == 1
+                    outer(1)
+                else
+                    1
+                end
+            end
+        end
+end
+
+
+
+
+
+module ExceptionHandlingTests
+
+    include("Exceptional.jl")
+    using Main.ExceptionHandling, .TestFunctionProvider, Test
+
+
+    @testset "Error Testset" begin
+        @test           reciprocal_error(42) == 1/42
+        @test_throws    DivisionByZero reciprocal_error(0)
+    end
+
+    @testset "Signal Testset" begin
+        @test   isnothing(reciprocal_signal(0))
+
+        # Signal detected
+        @test_logs (:info, "LineEndLimit detected") handling(LineEndLimit => (c)->@info "LineEndLimit detected") do
+                                                        print_line("Hello, everybody!!")
+                                                    end
+    end
+
+    @testset "Handling Testset" begin
+        # Dont exit after first handler (I saw a division by zero)
+        @test_logs (:info,"I saw it too")    handling(DivisionByZero =>
+                                                    (c)->@info "I saw it too") do
+                                                handling(DivisionByZero =>
+                                                        (c)->println("I saw a division by zero")) do
+                                                    reciprocal_signal(0)
+                                                end
+                                            end
+        
+        # Same behavior for parent type: Exception
+        @test_logs (:info,"I saw it too")    handling(Exception =>
+                                            (c)->@info "I saw it too") do
+                                        handling(Exception =>
+                                                (c)->println("I saw a division by zero")) do
+                                            reciprocal_signal(0)
+                                        end
+                                    end
+    end
+
+
+
+    @testset "Restart Testset" begin
+        @test   handling(DivisionByZero => (c)->invoke_restart(:return_zero)) do
+                    reciprocal_restart(0)
+                end == 0
+
+        @test   handling(DivisionByZero => (c)->invoke_restart(:return_value, 42)) do
+                    reciprocal_restart(0) == 42
+                end
+
+        @test   handling(DivisionByZero => (c)->invoke_restart(:retry_using, 99)) do
+                    reciprocal_restart(0) == reciprocal_restart(99)
+                end
+    end
+
+
+
+    @testset "Escape Testset" begin
+        @test mystery(0) == 3
+        @test mystery(1) == 2
+        @test mystery(2) == 4
+
+        # Escape with "Done"
+        @test   to_escape() do exit
+                    handling(DivisionByZero =>
+                        (c)->(println("I saw it too"); exit("Done"))) do
+                            handling(DivisionByZero =>
+                                (c)->println("I saw a division by zero")) do
+                                    reciprocal_error(0)
+                            end
+                    end
+                end == "Done"
+        
+        # Escape after second handler(I saw it too)
+        @test_logs (:info,"I saw it too")   to_escape() do exit
+                                                handling(DivisionByZero =>
+                                                    (c)->(@info "I saw it too"; exit("Done"))) do
+                                                        handling(DivisionByZero =>
+                                                            (c)->println("I saw a division by zero")) do
+                                                                reciprocal_error(0)
+                                                        end
+                                                end
+                                            end
+
+        # Escape after first handler(I saw a division by zero)
+        @test_logs (:info,"I saw a division by zero")   to_escape() do exit
+                                                            handling(DivisionByZero =>
+                                                                (c)->println("I saw it too")) do
+                                                                    handling(DivisionByZero =>
+                                                                        (c)->(@info "I saw a division by zero";
+                                                                            exit("Done"))) do
+                                                                                reciprocal_error(0)
+                                                                    end
+                                                            end
+                                                        end
+    end
+
+
+
+    @testset "Available Restart Testset" begin
+        # No restart available => throw error
+        @test_throws DivisionByZero handling(DivisionByZero =>
+                    (c)-> for restart in (:return_one, :return_zero, :die_horribly)
+                        if available_restart(restart)
+                            invoke_restart(restart)
+                        end
+                    end) do
+                    reciprocal_error(0)
+                end
+
+        @test_throws DivisionByZero handling(DivisionByZero =>
+                (c)-> for restart in (:return_one, :return_zero, :die_horribly)
+                    if available_restart(restart)
+                        invoke_restart(restart)
+                    end
+                end) do
+                reciprocal_error(0)
+            end
+    end
+end
--- Exceptional.jl
+++ Exceptional.jl
@@ -1,101 +1,110 @@
-# Define exception types
-struct DivisionByZero <: Exception end
-struct LineEndLimit <: Exception end
-struct BadLetter <: Exception end
-struct EscapeException <: Exception
-    id
-    value
-end
 
-# Define global stacks for restarts and handlers
-const RESTART_STACK = Vector{Vector{Pair{Symbol, Function}}}()
-const HANDLER_STACK = Vector{Vector{Pair{Type{<:Exception}, Function}}}()
+# provides functionality for exception handling
+module ExceptionHandling
 
-# Execute most recent handler
-function signal(e::Exception, mustBeHandled::Bool = false)
-    for frame in reverse(HANDLER_STACK)  # Reverse to get most recent
-        for (exc_type, handler) in frame
-            if e isa exc_type
-                handler(e)
-                break  # break after handling
-            end
-        end
-    end
-    if mustBeHandled
-        throw(e)
-    end
-end
+    export signal, error, handling, to_escape, with_restart, invoke_restart, available_restart
 
-# Signal an error that must be handled
-function error(e::Exception)
-    signal(e, true)
-end
-
-# Add handler to handlerstack 
-function handling(f::Function, handlers...) 
-    push!(HANDLER_STACK, collect(handlers))  # Push stackframe onto stack
-    try
-        return f()
-    finally
-        pop!(HANDLER_STACK)  # Restore previous state by popping the stackframe
-    end
-end
-
-#Take an anonymous function which has the named exit as an arugment with a return value
-function to_escape(f::Function)
-    id = gensym() 
-    try
-        return f(x -> throw(EscapeException(id, x)))  
-    catch e
-        if e isa EscapeException && e.id == id 
-            return e.value  # Return the escaped value
-        else
-            rethrow()
-        end
-    end
-end
-
-# Writing restarts on stack with defined exit points
-function with_restart(f::Function, restarts...)
-   to_escape() do exit
-        # map callback to anonymous function that calls exit with callback(args...) to create non-local transer of control point
-        new_restarts = map(((name, callback), ) -> name => (args...) -> exit(callback(args...)), restarts) 
-        push!(RESTART_STACK, collect(new_restarts))
-        try
-            return f()
-        finally
-            pop!(RESTART_STACK)  # Restore previous state by popping the stackframe
-        end
-    end
-end
-
-# Check if a restart is available
-function available_restart(name::Symbol)
-    for frame in reverse(RESTART_STACK)
-        for (restart_name, _) in frame
-            if name  === restart_name
-                return true
-            end
-        end
+    struct EscapeException <: Exception
+        id
+        value
     end
 
-    return false
-end
+    # Define global stacks for restarts and handlers
+    const RESTART_STACK = Vector{Vector{Pair{Symbol, Function}}}()
+    const HANDLER_STACK = Vector{Vector{Pair{Type{<:Exception}, Function}}}()
 
-#If restart available from stack, return the callback with arugments
-function invoke_restart(name::Symbol, args...)
-    if available_restart(name)
-        for frame in Iterators.reverse(RESTART_STACK)
-            for (restart_name, callback) in frame
-                if name  === restart_name
-                    callback(args...)
+
+
+    # Execute most recent handler
+    function signal(e::Exception, mustBeHandled::Bool = false)
+        for frame in reverse(HANDLER_STACK)  # Reverse order to obtain latest stackframe
+            for (exc_type, handler) in frame
+                if e isa exc_type
+                    handler(e)
+                    break  # break after handling
                 end
             end
         end
-    else
-        error("No callback found!")
+        if mustBeHandled
+            throw(e)
+        end
     end
+
+
+
+    # Signal an error that must be handled
+    function error(e::Exception)
+        signal(e, true)
+    end
+
+    # Add handler to handlerstack 
+    function handling(f::Function, handlers...) 
+        push!(HANDLER_STACK, collect(handlers))  # Push stackframe onto stack
+        try
+            return f()
+        finally
+            pop!(HANDLER_STACK)  # Restore previous state by popping the stackframe
+        end
+    end
+
+
+
+    #Take an anonymous function which has the named exit as an arugment with a return value
+    function to_escape(f::Function)
+        id = gensym() 
+        try
+            return f(x -> throw(EscapeException(id, x)))  
+        catch e
+            if e isa EscapeException && e.id == id 
+                return e.value  # Return the escaped value
+            else
+                rethrow()
+            end
+        end
+    end
+
+
+
+    # Provide restarts with defined exit points
+    function with_restart(f::Function, restarts...)
+    to_escape() do exit
+            # map callback to anonymous function that calls exit with callback(args...) to create non-local transer of control
+            new_restarts = map(((name, callback), ) -> name => (args...) -> exit(callback(args...)), restarts) 
+            push!(RESTART_STACK, collect(new_restarts))
+            try
+                return f()
+            finally
+                pop!(RESTART_STACK)  # Remove last stackframe
+            end
+        end
+    end
+
+    function available_restart(name::Symbol)
+        for frame in reverse(RESTART_STACK)
+            for (restart_name, _) in frame
+                if name  === restart_name
+                    return true
+                end
+            end
+        end
+
+        return false
+    end
+
+    # If restart available, return callback with arguments
+    function invoke_restart(name::Symbol, args...)
+        if available_restart(name)
+            for frame in Iterators.reverse(RESTART_STACK)
+                for (restart_name, callback) in frame
+                    if name  === restart_name
+                        callback(args...)
+                    end
+                end
+            end
+        else
+            error("No callback found!")
+        end
+    end
+
 end
-
-
 
